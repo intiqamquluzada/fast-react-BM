65c65c55e9ff298ac316b576a4d89fe619eb1292
Intigam Guluzade <quluzadintiqam@gmail.com>
2025-05-15 19:02:05 +0400

    base url for prod

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-export const API_BASE = "http://localhost:8000";
+export const API_BASE = "https://fast-react-bm.onrender.com";
 
 export const tasksAPI = {
   async getTasks(token) {
